2a6387bf11ad3d91404e1bc8a28d86e1707b73c2
Natthapol-PEET <natthapol.n@ku.th>
2022-06-28 16:43:09 +0700

    update

--- config.py
+++ config.py
@@ -1,23 +1,35 @@
 import network
 from machine import Pin
+import time
 
-ap_ssid = "Atom_WifiManager"
-ap_password = "12345678"
-ap_authmode = 3  # WPA2
+ssid = 'ARTANI_BUILDING_TWO_2G'
+password = 'TEDEls@84'
+# mqtt_server = 'broker.hivemq.com'
+mqtt_server = '192.168.1.85'
+#EXAMPLE IP ADDRESS
+client_id = 'esp32-atom'
+topic_sub = b'/mqtt/gate'
+topic_pub = b'notification'
+port = 1883
 
-NETWORK_PROFILES = 'wifi.dat'
-STATIC_IP_PROFILES = 'static_ip.dat'
+station = network.WLAN(network.STA_IF)
 
-wlan_ap = network.WLAN(network.AP_IF)
-wlan_sta = network.WLAN(network.STA_IF)
-
-server_socket = None
-
-OPEN = 1
-CLOSE = 0
-relay1 = Pin(22, Pin.OUT)
-relay2 = Pin(19, Pin.OUT)
+station.active(True)
+station.connect(ssid, password)
 
 button = Pin(39, Pin.IN, Pin.PULL_UP)
 
+OPEN = 0
+CLOSE = 1
+relay1 = Pin(22, Pin.OUT)
+relay2 = Pin(19, Pin.OUT)
 
+# init relay off
+relay1.value(CLOSE)
+relay2.value(CLOSE)
+
+time.sleep(0.5)
+relay2.value(OPEN)
+time.sleep(1)
+relay2.value(CLOSE)
+    
--- main.py
+++ main.py
@@ -1,70 +1,98 @@
-from config import *
-import wifi_manager
+# Complete project details at https://RandomNerdTutorials.com
+
 import time
+from config import *
 import led
-import _thread
+from umqttsimple import MQTTClient
+import ubinascii
+import machine
+import micropython
+import network
+import esp
+esp.osdebug(None)
+import gc
+gc.collect()
 
 
-def worker():
-    # check connection wifi
-    _thread.start_new_thread(wifi_manager.check_connection, ())
-
-    # start server
-    wifi_manager.start()
-
-
-def main():
-    '''
-        send_header
-        send_response
-        handle_root
-        handle_configure
-        handle_not_found
-        stop
-        start
-    '''
+def main():    
+    # wait intenet connection 
+    led.red()
     
-    # init relay off
+    cnt = 0
+    print('Trying to connect to %s...' % ssid)
+    while station.isconnected() == False:
+      # print('.', end='')
+      # time.sleep(0.5)
+      cnt += 1
+      if cnt > 5000000:
+          print('*---------- reset ----------*')
+          machine.reset()
+
+    # intenet connected
+    led.yellow()
+
+    print('Connection successful')
+    print(station.ifconfig())
+
+    try:
+      client = connect_and_subscribe()
+    except OSError as e:
+      restart_and_reconnect()
+
+    while True:
+      try:
+        if station.isconnected() == False:
+            led.blue()
+            restart_and_reconnect()
+        # new_message = client.check_msg()
+        # if new_message != 'None':
+        #   client.publish(topic_pub, b'received')
+        time.sleep(1)
+      except KeyboardInterrupt:
+        break
+      except OSError as e:
+        restart_and_reconnect()
+
+
+def restart_and_reconnect():
+  print('Failed to connect to MQTT broker. Reconnecting...')
+  time.sleep(5)
+  machine.reset()
+  
+
+def connect_and_subscribe():
+  client = MQTTClient(client_id=client_id, server=mqtt_server, port=port)
+  client.set_callback(sub_cb)
+  client.connect()
+  client.subscribe(topic_sub)
+  print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_sub))
+  
+  # connect mqtt
+  led.green()
+  
+  return client
+  
+  
+def sub_cb(topic, msg):
+  print((topic, msg.decode("utf-8")))
+  
+  if msg.decode("utf-8") == 'open':
+      remoteRelay1()
+  elif msg.decode("utf-8") == 'close':
+      remoteRelay2()
+      
+def remoteRelay1():
+    relay1.value(OPEN)
+    time.sleep(1)
     relay1.value(CLOSE)
+
+
+def remoteRelay2():
+    relay2.value(OPEN)
+    time.sleep(1)
     relay2.value(CLOSE)
 
-    # manual start
-    fixip_wait = True
-    button_state = False
-    cnt = 0
 
-    # wait led status
-    led.yellow()
-    print('wait button setup network', end='')
 
-    while fixip_wait and cnt < 10:
-        if not button.value():
-            led.blue()
-            fixip_wait = False
-            button_state = True
-
-            # setup network, fix network and run server
-            worker()
-        
-        print('.', end='')
-        cnt = cnt + 1
-        time.sleep(0.5)
-        led.red()
-        time.sleep(0.5)
-        led.yellow()
-
-    if not button_state:
-        # auto start
-        profiles = wifi_manager.read_profiles()
-        static_network = wifi_manager.read_static_profiles()
-
-        print(f"profiles: {profiles}")
-        print(f"static_network: {static_network}")
-
-        for ssid, password in profiles.items():
-            wifi_manager.do_connect_fixip(ssid, password, static_network)
-
-        # run server
-        worker()
 
 
